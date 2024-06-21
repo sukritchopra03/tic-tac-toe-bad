8c602d0983f68696c096b17513c5b1db53607a34
sukritchopra03 <sukrit.chopra.032@adgitmdelhi.ac.in>
2024-06-21 17:18:51 +0530

    hello

---
+++ random.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Include the iostream header for cout
+
+using namespace std;
+
+int main() {
+        
+    cout << "helklklklklo world" << endl; // Print "hello world" and move to the next line
+  
+    
+    return 0; // Return 0 to indicate successful execution
+}
